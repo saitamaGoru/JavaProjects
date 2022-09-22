d7321580fec34ff2dc1cf3ca257847a0a79fd5ae
pgrewa26 <300961555@cencol.ca>
2022-09-22 08:51:49 -0400

    wk3-sessin-1 created

--- src/PrimitiveTypes/PrimitiveTypes.java
+++ src/PrimitiveTypes/PrimitiveTypes.java
@@ -5,7 +5,14 @@ public class PrimitiveTypes {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		boolean aBool = true;
-		char a
+		char aChar = 'a';
+		float aNumber = 32.2f;
+		long aWholeNumber = 88;
+		System.out.println(aBool);
+		System.out.println(aChar);
+		System.out.println(aNumber);
+		System.out.println(aWholeNumber);
+		
 	}
 
 }
