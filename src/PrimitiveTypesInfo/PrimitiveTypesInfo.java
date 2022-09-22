a41632de05f8f3bf1165f46a4f736abdb8d1bd40
pgrewa26 <300961555@cencol.ca>
2022-09-22 09:30:17 -0400

    added new packages

---
+++ src/PrimitiveTypesInfo/PrimitiveTypesInfo.java
@@ -0,0 +1,17 @@
+package PrimitiveTypesInfo;
+
+public class PrimitiveTypesInfo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		
+		float aPrimitive = 32.2f;
+		System.out.println("Value:               "+aPrimitive);
+		System.out.println("Type:                "+Float.TYPE);
+		System.out.println("Size in memeory:     "+Float.SIZE + "bits.");
+		System.out.println("Minimum Value:       "+Float.MIN_VALUE);
+		System.out.println("Maximum Value:       "+Float.MAX_VALUE);
+	}
+
+}
---
+++ src/StringExample/StringExample.java
@@ -0,0 +1,16 @@
+package StringExample;
+
+public class StringExample {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String first = "COMP228";
+		String second = "JAVA";
+		String third= "FUN";
+		
+		System.out.println(first);
+		System.out.println(second);
+		System.out.println(third);
+	}
+
+}
